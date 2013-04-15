369312ebe8dfc17824144f63882ce6cf6299972f
akanksha <akanksha@aka.(none)>
2013-04-15 23:07:27 +0530

    new version with compiled modifications uploaded

---
+++ ShowGraphics.hs
@@ -0,0 +1,49 @@
+{- |
+Module      :  <File name or $Header$ to be replaced automatically>
+Description :  <optional short text displayed on contents page>
+Copyright   :  (c) <Authors or Affiliations>
+License     :  <license>
+
+Maintainer  :  <email>
+Stability   :  unstable | experimental | provisional | stable | frozen
+Portability :  portable | non-portable (<reason>)
+
+<module description starting at first column>
+-}
+module ShowGraphics  where
+import Graphics.UI.GLUT
+import System.IO  
+import Control.Monad
+import Drawbox
+import ReadnSend
+-- | Takes input text file and creates the window for showing graphics
+display = do
+    
+     handle <- openFile "writefile.txt" ReadMode
+     contents <- hGetContents handle
+     let list = filter (/="  ") $ filter (/="") $ filter (/=" ") $ lines contents
+          
+     print list    
+     hClose handle
+     getArgsAndInitialize
+     createWindow ""
+     windowSize $= Size 1024 700
+     clear [ColorBuffer]
+     
+     displayCallback $= readNsend list --[ "select","ns"]
+     reshapeCallback $= Just reshape
+     --actionOnWindowClose $= ContinueExectuion
+     mainLoop
+reshape s@(Size w h) = do
+  viewport $= (Position 0 0, s)
+  postRedisplay Nothing
+
+{--
+pointsl::[(GLfloat,GLfloat,GLfloat)]    
+pointsl =
+    [(-0.8, 0, 0.0)
+    ,(-0.8, -0.2, 0.0)
+    ,(-0.5, -0.2, 0.0)
+    ,((-0.5), 0, 0.0)] 
+--}
+
